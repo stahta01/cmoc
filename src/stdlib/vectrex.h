7c100f5241af42e8d2891865798b8e4dc67c4918
Tim Stahlhut <stahta01@gmail.com>
2018-11-06 19:24:47 -0500

    Moved or delete vectrex files to match cmoc-0.1.54

--- src/stdlib/vectrex.h
+++ src/stdlib/vectrex.h
@@ -3,8 +3,7 @@
 #ifndef __vectrex_h__
 #define __vectrex_h__
 
-#define FALSE 0
-#define TRUE  1
+enum { FALSE, TRUE };
 
 #ifndef _CMOC_BASIC_TYPES_
 #define _CMOC_BASIC_TYPES_
@@ -13,6 +12,8 @@ typedef unsigned char byte;
 typedef signed char   sbyte;
 typedef unsigned int  word;
 typedef signed int    sword;
+typedef unsigned long dword;
+typedef signed long   sdword;
 
 #endif
 
@@ -20,5 +21,7 @@ typedef unsigned char uint8_t;
 typedef signed char   int8_t;
 typedef unsigned int  uint16_t;
 typedef signed int    int16_t;
+typedef unsigned long uint32_t;
+typedef signed long   int32_t;
 
 #endif
